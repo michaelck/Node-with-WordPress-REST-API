febd2bda45a933cc7b0ac5f0f406266ed9a97d7a
Michael <kappeler.michael@gmail.com>
2015-12-11 21:29:52 -0500

    Adds basic route functionality

--- index.js
+++ index.js
@@ -12,7 +12,29 @@ app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res){
-  res.render('layout');
+  var docs;
+  var options = {
+  host: config.wpUrl,
+  port: 80,
+  path: '/wp-json/wp/v2/posts/',
+  method: 'GET'
+  };
+
+  http.request(options, function(resp) {
+    var docs;
+    resp.setEncoding('utf8');
+     resp.on('data', function (chunk) {
+       if (docs === undefined){
+         docs = chunk;
+       } else{
+         docs +=chunk;
+       }
+     });
+     resp.on('end', function(){
+       var jdocs = JSON.parse(docs);
+       res.render('layout', {posts:jdocs});
+     });
+  }).end();
 });
 
 app.listen(config.listen)
